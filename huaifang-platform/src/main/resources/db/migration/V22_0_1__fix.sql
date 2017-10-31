8f4eb8712331127d230f6d96f4aedc5d6245a7d4
chenJz1012 <597160667@qq.com>
2017-10-31 21:27:15 +0800

    ``

--- huaifang-platform/src/main/resources/db/migration/V22_0_1__fix.sql
+++ huaifang-platform/src/main/resources/db/migration/V22_0_1__fix.sql
@@ -4,8 +4,8 @@ COMMENT '用户昵称';
 
 ALTER TABLE d_app_manager
   ADD COLUMN current INT(11) DEFAULT 0
-COMMENT '是否当前版本';
+COMMENT '是否当前版本:select:[0#否,1#是]';
 
 ALTER TABLE d_app_manager
   ADD COLUMN force_update INT(11) DEFAULT 0
-COMMENT '是否强制更新';
+COMMENT '是否强制更新:select:[0#否,1#是]';
